36ad17ca7cf87b047595e3cd05be67b6cdd06ac3
SaulCM <saulcaspamiranda@gmail.com>
2019-05-13 04:14:03 -0400

    modulo registrar y componente formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  {
-    path: 'store',
-    loadChildren: './store/store.module#StoreModule'
+    {path: 'store',
+    loadChildren: './store/store.module#StoreModule'},
+    {path: 'registrar',
+    loadChildren: './registrar/registrar.module#RegistrarModule'},
     // canActivate: [AuthGuard]
-  }
 ];
 
 @NgModule({
